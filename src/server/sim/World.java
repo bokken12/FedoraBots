ae23abec40e03f32a84e691945b4e676f0ec16a5
Ryan Adolf <ryanadolf123@gmail.com>
2017-07-26 21:11:42 -0700

    Remove unused import in World.java

--- src/server/sim/World.java
+++ src/server/sim/World.java
@@ -6,7 +6,6 @@ package server.sim;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Stack;
