a26f112795c0cad5a778f953bd519af8c57f4844
Ryan Adolf <ryanadolf123@gmail.com>
2017-08-21 21:42:27 -0700

    Change cli debug level back to INFO

--- src/fedorabots/server/Cli.java
+++ src/fedorabots/server/Cli.java
@@ -22,7 +22,7 @@ import fedorabots.server.sim.world.World;
 
 public class Cli {
 
-    private static final Level LOG_LEVEL = Level.FINER;
+    private static final Level LOG_LEVEL = Level.INFO;
 
     static {
         System.setProperty("java.util.logging.SimpleFormatter.format", "%1$tY-%1$tm-%1$td %1$tH:%1$tM:%1$tS %4$s %2$s %5$s%6$s%n");
